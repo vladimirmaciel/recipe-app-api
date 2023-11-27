3ab11ab828b1a4a7b982e1304ed2bd08174c88ad
Vlamidir Serra <vladimirmaciel@gmail.com>
2023-11-27 16:06:51 -0300

    configurando projeto app e arquivos necessarios para containner docker

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract y from x and return result."""
+    return x - y
+
+def divide(x, y):
+    """Divide x by y and return result."""
+    return x / y
