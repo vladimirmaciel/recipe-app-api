e44896d0b05bf82519c7cd4d463fb0f1f6fd7399
Vlamidir Serra <vladimirmaciel@gmail.com>
2023-12-03 16:03:28 -0300

    teste e pagina do admin do django

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,43 @@
-from django.contrib import admin  # noqa: F401
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """User admin class"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (_('Permissions'), {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None,
+         {'classes': ('wide',),
+          'fields': (
+              'email',
+              'password1',
+              'password2',
+              'name',
+              'is_staff',
+              'is_superuser',
+              'is_active',
+          )
+          }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
