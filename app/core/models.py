c5b4217bfae4a3c006bcfbe92ee919e9ac953b53
Vlamidir Serra <vladimirmaciel@gmail.com>
2023-12-03 01:06:19 -0300

    creado teste, verificando  cadastro de usario passando email e senha

--- app/core/models.py
+++ app/core/models.py
@@ -8,24 +8,28 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     """User manager class"""
+
     def create_user(self, email, password=None, **extra_fields):
         """Create and save a new user"""
         if not email:
             raise ValueError('User must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
-        user.set_password(password) # encrypt password
-        user.save(using=self._db) # support multiple databases
+        user.set_password(password)  # encrypt password
+        user.save(using=self._db)  # support multiple databases
         return user
 
     def create_superuser(self, email, password):
         """Create and save a new superuser"""
         user = self.create_user(email, password)
-        user.is_superuser = True # is_superuser is created by PermissionsMixin
-        user.is_staff = True # is_staff is created by PermissionsMixin
+        user.is_superuser = True  # is_superuser is created by PermissionsMixin
+        user.is_staff = True  # is_staff is created by PermissionsMixin
         user.save(using=self._db)
         return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
