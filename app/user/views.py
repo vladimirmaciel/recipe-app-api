0ee02e560048372b4af79ec26bc4f934c629fe09
Vlamidir Serra <vladimirmaciel@gmail.com>
2023-12-12 01:45:39 -0300

    feat: gerencia api usuario e utilizacao de token

--- app/user/views.py
+++ app/user/views.py
@@ -21,6 +21,7 @@ class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user"""
     serializer_class = UserSerializer
@@ -34,6 +35,3 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         """Retrieve and return authenticated user"""
         return self.request.user
-
-
-
