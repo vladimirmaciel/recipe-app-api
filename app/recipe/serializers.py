76235730e93f419ba821d1603756a2c071c87248
Vlamidir Serra <vladimirmaciel@gmail.com>
2023-12-26 22:46:24 -0300

    feat(test): Test for list recipes

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,21 +1,22 @@
 """
-serializer for recipe API
+Serializers for recipe APIs
 """
-
 from rest_framework import serializers
+
 from core.models import Recipe
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    """Serializer for recipe objects"""
+    """Serializer for recipes."""
 
     class Meta:
         model = Recipe
-        fields = (
-            'id',
-            'title',
-            'time_minutes',
-            'price',
-            'link',
-        )
-        read_only_fields = ('id',)
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
