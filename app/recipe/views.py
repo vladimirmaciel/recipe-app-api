9ddb4286d98aabe9c7304c9825830fde6a3b6d4c
Vlamidir Serra <vladimirmaciel@gmail.com>
2024-02-07 11:42:53 -0300

    implement ingredient listing API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -10,6 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
 from recipe import serializers
 
@@ -49,4 +50,17 @@ class TagViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Retrieve tags for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        viewsets.mixins.ListModelMixin):
+    """View for manage ingredients APIs."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve ingredients for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
